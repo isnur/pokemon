4e47f0d4aec6ff49af43b872f553975e70cf0376
isnur <isnurmsm@gmail.com>
2019-09-11 02:07:25 +0700

    test(card): create tests

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,30 +9,32 @@ const areEqual = (prevProps, nextProps) => {
 };
 
 const card = (props) => {
-  const removeButton = props.removed ?
-    <div className="card__content--delete" onClick={props.removed} title="Release Pokemon">
-      <img src="/images/pokeball.svg" className="card__image" alt="Release" />
-      <span>Release</span>
-    </div> : null;
-  const cardContent = !props.clicked ?
-    <div className="card__content">
-      <div className="card__content--description">
-        {capitalizeFirstLetters(props.name)}
+  if (props.name) {
+    const removeButton = props.removed ?
+      <div className="card__content--delete" onClick={props.removed} title="Release Pokemon">
+        <img src="/images/pokeball.svg" className="card__image" alt="Release" />
+        <span>Release</span>
+      </div> : null;
+    const cardContent = !props.clicked ?
+      <div className="card__content">
+        <div className="card__content--description">
+          {capitalizeFirstLetters(props.name)}
+        </div>
+      </div> :
+      <Link className={`card__content${props.removed ? ' card__content--link-remove' : ' card__content--link'}`}
+        to={`/detail/${props.id}`}>
+        <div className="card__content--description">
+          {capitalizeFirstLetters(props.name)}
+        </div>
+      </Link>;
+    return (
+      <div className="card">
+        {cardContent}
+        {removeButton}
       </div>
-    </div> :
-    <Link className={`card__content${props.removed ? ' card__content--link-remove' : ' card__content--link'}`}
-      to={`/detail/${props.id}`}>
-      <div className="card__content--description">
-        {capitalizeFirstLetters(props.name)}
-      </div>
-    </Link>;
-
-  return (
-    <div className="card">
-      {cardContent}
-      {removeButton}
-    </div>
-  );
+    );
+  }
+  return null;
 };
 
 export default memo(card, areEqual);
---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import Card from './Card';
+
+describe('<Card />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Card />);
+  });
+
+  it('should return null', () => {
+    expect(wrapper.type()).toEqual(null);
+  });
+
+  it('should render an `.card`', () => {
+    wrapper.setProps({ name: 'nidoran-m' });
+    expect(wrapper.find('.card')).toHaveLength(1);
+  });
+
+  it('should an exact formated name', () => {
+    wrapper.setProps({ name: 'nidoran-m' });
+    const content = wrapper.find('.card__content--description');
+    expect(content.text()).toEqual('Nidoran M');
+  });
+
+  it('should render remove button', () => {
+    wrapper.setProps({ name: 'nidoran-m', removed: true });
+    expect(wrapper.find('.card__content--delete')).toHaveLength(1);
+  });
+
+  it('should render card content', () => {
+    wrapper.setProps({ name: 'nidoran-m' });
+    expect(wrapper.find('.card__content')).toHaveLength(1);
+  });
+
+  it('should render clickable card content with full width style', () => {
+    wrapper.setProps({ name: 'nidoran-m', clicked: true });
+    expect(wrapper.find('.card__content.card__content--link')).toHaveLength(1);
+  });
+
+  it('should render clickable card content with not full width style', () => {
+    wrapper.setProps({ name: 'nidoran-m', removed: true, clicked: true });
+    expect(wrapper.find('.card__content.card__content--link-remove')).toHaveLength(1);
+  });
+});
