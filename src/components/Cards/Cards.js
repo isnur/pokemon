77090d8607cf2ca4a954ef5c0ac2f434fbc74eea
isnur <isnurmsm@gmail.com>
2019-09-11 02:07:42 +0700

    test(cards): create tests

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -6,18 +6,21 @@ import { getIdFromUrl } from '../../helpers';
 import Spinner from '../Spinner/Spinner';
 
 const cards = (props) => {
-  let cardItems = props.items.map(pokemon =>
-    <Card key={getIdFromUrl(pokemon.url)}
-      id={getIdFromUrl(pokemon.url)}
-      name={pokemon.nickname ? pokemon.nickname : pokemon.name}
-      removed={props.removed ? () => props.removed(getIdFromUrl(pokemon.url)) : null}
-      clicked={props.clicked}
-    />);
+  let cardItems = null;
   if (props.isLoading) {
     cardItems = <Spinner radius="10" strokeWidth="1" color="#03ac0e" />;
-  }
-  if (!props.isLoading && cardItems.length === 0) {
-    cardItems = <p>No data</p>;
+  } else {
+    if (props.items && props.items.length !== 0) {
+      cardItems = props.items.map(pokemon =>
+        <Card key={getIdFromUrl(pokemon.url)}
+          id={getIdFromUrl(pokemon.url)}
+          name={pokemon.nickname ? pokemon.nickname : pokemon.name}
+          removed={props.removed ? () => props.removed(getIdFromUrl(pokemon.url)) : null}
+          clicked={props.clicked}
+        />);
+    } else {
+      cardItems = <p>No data</p>;
+    }
   }
   return (
     <div className="cards">
---
+++ src/components/Cards/Cards.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import Cards from './Cards';
+import Card from '../Card/Card';
+import Spinner from '../Spinner/Spinner';
+
+describe('<Cards />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Cards />);
+  });
+
+  it('should render an `.cards`', () => {
+    expect(wrapper.find('.cards')).toHaveLength(1);
+  });
+
+  it('should an exact loading spinner', () => {
+    wrapper.setProps({ isLoading: true });
+    expect(wrapper.contains(<Spinner radius="10" strokeWidth="1" color="#03ac0e" />)).toEqual(true);
+  });
+
+  it('should an exact No data', () => {
+    expect(wrapper.contains(<p>No data</p>)).toEqual(true);
+  });
+
+  it('should render 2 <Card /> elements', () => {
+    const items = [
+      { "name": "bulbasaur", "url": "https://pokeapi.co/api/v2/pokemon/1/" },
+      { "name": "ivysaur", "url": "https://pokeapi.co/api/v2/pokemon/2/" }
+    ];
+    wrapper.setProps({ items: items });
+    expect(wrapper.find(Card)).toHaveLength(2);
+  });
+});
