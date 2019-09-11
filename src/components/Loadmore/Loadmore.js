455acccaa3bbf858349d5eb3b8f88872f64b69d7
isnur <isnurmsm@gmail.com>
2019-09-11 16:35:23 +0700

    test(loadmore): Create tests

--- src/components/Loadmore/Loadmore.js
+++ src/components/Loadmore/Loadmore.js
@@ -7,7 +7,7 @@ function areEqual(prevProps, nextProps) {
   return prevProps.isLoading === nextProps.isLoading;
 }
 const loadmore = (props) => {
-  return (<button className="card" onClick={props.loadmore} disabled={props.isLoading}>
+  return (<button className="card" onClick={props.loadmore} disabled={props.isLoading ? props.isLoading : false}>
     <span className="card__content card__content--loadmore">
       Load more{props.isLoading ? <>&nbsp;<Spinner radius="30" width="16px" height="16px" strokeWidth="5" color="#03ac0e" /></> : null}
     </span>
---
+++ src/components/Loadmore/Loadmore.test.js
@@ -0,0 +1,42 @@
+import React, { Memo } from 'react';
+
+import { shallow } from 'enzyme';
+
+import Loadmore from './Loadmore';
+import Spinner from '../Spinner/Spinner';
+import { fetchPokemonList } from '../../containers/Home/Home';
+
+describe('<Loadmore />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Loadmore />);
+  });
+
+  it('should render an `.card`', () => {
+    expect(wrapper.find('.card')).toHaveLength(1);
+  });
+
+  it('should disabled button when isLoading `true`', () => {
+    wrapper.setProps({ isLoading: true, disabled: true });
+    expect(wrapper.prop('disabled')).toEqual(true);
+  });
+
+  it('should an exact loading spinner', () => {
+    wrapper.setProps({ isLoading: true });
+    expect(wrapper.contains(<Spinner radius="30" width="16px" height="16px" strokeWidth="5" color="#03ac0e" />)).toEqual(true);
+  });
+
+  it('should called function when clicked', () => {
+    const mock = jest.fn();
+    const wrapper = shallow(
+      <Loadmore
+        nextUrl="https://pokeapi.co/api/v2/pokemon/?offset=25&limit=25"
+        isLoading="false"
+        loadmore={mock}
+      />
+    )
+    wrapper.simulate('click');
+    expect(mock).toHaveBeenCalled();
+  });
+});
